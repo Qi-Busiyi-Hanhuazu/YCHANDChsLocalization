88581b2ec55a5ced7d9f55aa39f9c605c2094896
Xzonn <Xzonn@outlook.com>
2024-04-22 01:00:19 +0800

    Update workflow and MT

--- scripts/convert_csv_to_json.py
+++ scripts/convert_csv_to_json.py
@@ -1,7 +1,11 @@
+from collections.abc import Callable
 import csv
 import hashlib
 import json
 import os
+import re
+import shutil
+from typing import Any
 
 LANGUAGE = os.getenv("XZ_LANGUAGE") or "zh_Hans"
 
@@ -10,96 +14,113 @@ DIR_JSON_TRANSLATED = f"texts/{LANGUAGE}"
 DIR_CSV_TRANSLATED = f"texts/{LANGUAGE}"
 
 
-def get_sheet(sheet_name: str) -> dict[str, str]:
-  csvfile = open(f"{DIR_CSV_TRANSLATED}/{sheet_name}.csv", "r", -1, "utf8", "ignore", "")
-  reader = csv.reader(csvfile)
+def handle_json(sheet_name: str, handler: Callable[[dict[str, str], Any], Any]) -> bool:
+  if not os.path.exists(f"{DIR_CSV_TRANSLATED}/{sheet_name}.csv") or not os.path.exists(
+      f"{DIR_JSON_ORIGINAL}/{sheet_name}.json"):
+    return False
 
-  row_iter = reader
-  headers = next(row_iter)
-  translations: dict[str, str] = {}
-  for row in row_iter:
-    item_dict = dict(zip(headers, row))
-    translations[item_dict["id"]] = item_dict["target"].replace("\\r", "\r")
-
-  return translations
-
-
-# Scripts
-os.makedirs(f"{DIR_JSON_TRANSLATED}/scrpt.cpk", exist_ok=True)
-for file_name in os.listdir(f"{DIR_JSON_ORIGINAL}/scrpt.cpk"):
-  if not file_name.endswith(".json"):
-    continue
-
-  with open(f"{DIR_JSON_ORIGINAL}/scrpt.cpk/{file_name}", "r", -1, "utf8") as reader:
+  with open(f"{DIR_JSON_ORIGINAL}/{sheet_name}.json", "r", -1, "utf8") as reader:
     data = json.load(reader)
-    messages = data["message"]
-  if len(messages) < 1:
-    continue
 
-  sheet_name = f"scrpt.cpk/{file_name.removesuffix('.json')}"
-  translations = get_sheet(sheet_name)
+  with open(f"{DIR_CSV_TRANSLATED}/{sheet_name}.csv", "r", -1, "utf8", "ignore", "") as csvfile:
+    reader = csv.reader(csvfile)
+
+    row_iter = reader
+    headers = next(row_iter)
+    translations: dict[str, str] = {}
+    for row in row_iter:
+      item_dict = dict(zip(headers, row))
+      translations[item_dict["id"]] = item_dict["target"].replace("\\r", "\r")
+
+  if not handler(translations, data):
+    return False
+
+  with open(f"{DIR_JSON_TRANSLATED}/{sheet_name}.json", "w", -1, "utf8", None, "\n") as writer:
+    json.dump(data, writer, ensure_ascii=False, indent=2)
+  print(f"Converted: {sheet_name}.json")
+  return True
+
+
+def scripts_handler(translations: dict[str, str], data: dict[str, Any]) -> bool:
+  messages = data["message"]
+  if len(messages) < 1:
+    return False
 
   for i, item in enumerate(messages):
     function = item["function"]
+    text_id = f"{function}_{i:04d}"
+    translation = translations.get(text_id, "")
     if function == "XMESS":
       text_id = f"{function}_{item['msgidx']:04d}"
       translation = translations[text_id]
       item["argument"] = translation.split("\n")
     elif function == "MSTD":
       for choice_i, choice in enumerate(item["argument"][3:]):
-        line, _, text_id = choice.split(",")
+        text_id = f"{function}_{i:04d}-{choice_i}"
+        line, _ = choice.split(",", 1)
         translation = translations[text_id]
-        item["argument"][3 + choice_i] = f"{translation},{_},{text_id}"
+        item["argument"][3 + choice_i] = f"{translation},{_}"
       continue
     else:
       continue
 
-  with open(f"{DIR_JSON_TRANSLATED}/scrpt.cpk/{file_name}", "w", -1, "utf8", "ignore", "\n") as writer:
-    json.dump(data, writer, ensure_ascii=False, indent=2)
+  return True
 
-  print(f"Converted: {file_name}")
 
-# OmakeData
-if True:
-  omake_translations = get_sheet("OmakeData")
-  file_name = "OmakeData_603d8b201237d579.json"
-  with open(f"{DIR_JSON_ORIGINAL}/{file_name}", "r", -1, "utf8") as reader:
-    data = json.load(reader)
-
+def omake_603d8b2_data_handler(translations: dict[str, str], data: dict[str, Any]) -> bool:
   for sublist in [x["items"] for x in data["pages"]]:
     for item in sublist:
-      item["title"] = omake_translations[item["title"]]
+      item["title"] = translations[f"omake#{item['ofs']:02d}"]
 
-  with open(f"{DIR_JSON_TRANSLATED}/{file_name}", "w", -1, "utf-8") as writer:
-    json.dump(data, writer, indent=2, ensure_ascii=False)
-  print(f"Converted: {file_name}")
+  return True
 
-  file_name = "OmakeData_d1cca4806d0942fd.json"
-  with open(f"{DIR_JSON_ORIGINAL}/{file_name}", "r", -1, "utf8") as reader:
-    data = json.load(reader)
 
+def omake_d1cca48_data_handler(translations: dict[str, str], data: dict[str, Any]) -> bool:
   for sublist in [x["items"] for x in data["pages"]]:
     for item in sublist:
-      title = item["title"].split("　")[-1]
-      item["title"] = item["title"].replace(title, omake_translations[title], 1)
+      title: str = item["title"]
+      ofs = re.sub(r"[０-９]", lambda x: f"{ord(x.group(0)) - ord('０')}", re.search(r"ＣＧ№([０-９]+)", title).group(1))
+      front, end = title.rsplit("　", 1)
+      end = translations[f"omake#{int(ofs) - 1:02d}"]
+      item["title"] = f"{front}　{end}"
 
-  with open(f"{DIR_JSON_TRANSLATED}/{file_name}", "w", -1, "utf-8") as writer:
-    json.dump(data, writer, indent=2, ensure_ascii=False)
-  print(f"Converted: {file_name}")
+  return True
 
-# Text
-if True:
-  text_translations = get_sheet("Text")
-  file_name = "Text.json"
-  with open(f"{DIR_JSON_ORIGINAL}/{file_name}", "r", -1, "utf8") as reader:
-    data = json.load(reader)
 
+def text_handler(translations: dict[str, str], data: dict[str, str]) -> bool:
   for k, v in data.items():
     md5 = hashlib.md5(k.encode("utf-8")).hexdigest()
-    if f"text#{md5}" not in text_translations:
+    if f"text#{md5}" not in translations:
       continue
-    data[k] = text_translations[f"text#{md5}"]
+    data[k] = translations[f"text#{md5}"]
 
-  with open(f"{DIR_JSON_TRANSLATED}/{file_name}", "w", -1, "utf-8") as writer:
-    json.dump(data, writer, indent=2, ensure_ascii=False)
-  print(f"Converted: {file_name}")
+  return True
+
+
+def metadata_handler(translations: dict[str, str], data: dict[str, dict[str, int | str]]) -> bool:
+  for i, item in enumerate(data):
+    item["text"] = translations[f"metadata#0x{item['position']:08x}+{item['length']}"]
+
+  return True
+
+
+os.makedirs(f"{DIR_JSON_TRANSLATED}/scrpt.cpk", exist_ok=True)
+for file_name in os.listdir(f"{DIR_JSON_ORIGINAL}/scrpt.cpk"):
+  if not file_name.endswith(".json"):
+    continue
+
+  handle_json(f"scrpt.cpk/{file_name.removesuffix('.json')}", scripts_handler)
+
+shutil.copyfile("original_files/json/OmakeData_603d8b201237d579.json", "original_files/json/OmakeData.json")
+handle_json("OmakeData", omake_603d8b2_data_handler)
+shutil.copyfile("texts/zh_Hans/OmakeData.json", "texts/zh_Hans/OmakeData_603d8b201237d579.json")
+
+shutil.copyfile("original_files/json/OmakeData_d1cca4806d0942fd.json", "original_files/json/OmakeData.json")
+handle_json("OmakeData", omake_d1cca48_data_handler)
+shutil.copyfile("texts/zh_Hans/OmakeData.json", "texts/zh_Hans/OmakeData_d1cca4806d0942fd.json")
+
+os.remove("original_files/json/OmakeData.json")
+os.remove("texts/zh_Hans/OmakeData.json")
+
+handle_json("Text", text_handler)
+handle_json("Metadata", metadata_handler)
